5ad2f0bb0923ac0ea743c809bc53e7ca76e8dd4a
Marcos Carrera <ramoscarrer@gmail.com>
2020-12-17 21:34:18 -0500

    updated tests

--- core/src/test/java/com/github/rcmarc/quadtree/core/QuadtreeTest.java
+++ core/src/test/java/com/github/rcmarc/quadtree/core/QuadtreeTest.java
@@ -42,7 +42,7 @@ class QuadtreeTest {
         assertEquals(Corner.BOTTOM_LEFT, tree.getCorner(point));
         point = new Point2D(5,5);
         assertEquals(Corner.BOTTOM_LEFT, tree.getCorner(point));
-        point = new Point2D(7.5,7.5);
+        point = new Point2D(10,10);
         assertEquals(Corner.MIDDLE, tree.getCorner(point));
         point = new Point2D(16,10);
         try {
@@ -56,7 +56,7 @@ class QuadtreeTest {
         point = new Point2D(5, 14);
         assertEquals(Corner.TOP_LEFT, tree.getCorner(point));
         point = new Point2D(14, 7.5);
-        assertEquals(Corner.TOP_RIGHT, tree.getCorner(point));
+        assertEquals(Corner.BOTTOM_RIGHT, tree.getCorner(point));
     }
 
     @Test
@@ -78,22 +78,26 @@ class QuadtreeTest {
 
         Quadtree<Integer> offset_tree = new Quadtree<>(new Point2D(10,10), new Point2D(5,5));
         offset_tree.subdivide();
-        assertEquals(offset_tree.quadrants[0], new Quadtree<>(new Point2D(5,5), new Point2D(5,7.5)));
-        assertEquals(offset_tree.quadrants[1], new Quadtree<>(new Point2D(5,5), new Point2D(7.5,7.5)));
+        assertEquals(offset_tree.quadrants[0], new Quadtree<>(new Point2D(5,5), new Point2D(5,10)));
+        assertEquals(offset_tree.quadrants[1], new Quadtree<>(new Point2D(5,5), new Point2D(10,10)));
         assertEquals(offset_tree.quadrants[3], new Quadtree<>(new Point2D(5,5), new Point2D(5,5)));
-        assertEquals(offset_tree.quadrants[2], new Quadtree<>(new Point2D(5,5), new Point2D(7.5,5)));
+        assertEquals(offset_tree.quadrants[2], new Quadtree<>(new Point2D(5,5), new Point2D(10,5)));
     }
 
     @Test
     public void testInsert(){
         Quadtree<Integer> tree = new Quadtree<>(new Point2D(10,10));
-        Point2D point = new Point2D(4,6);
-        tree.insert(new Data<>(5,point));
-        tree.insert(new Data<>(5,new Point2D(2,7)));
-        tree.insert(new Data<>(5,new Point2D(1,8)));
+        Point2D point_1 = new Point2D(4,6);
+        Point2D point_2 = new Point2D(2,7);
+        Point2D point_3 = new Point2D(1, 8);
+        tree.insert(new Data<>(5,point_1));
+        tree.insert(new Data<>(5,point_2));
+        tree.insert(new Data<>(5,point_3));
 
         assertTrue(Arrays.stream(tree.quadrants).allMatch(Objects::nonNull));
-        assertEquals(new Quadtree<Integer>(new Point2D(2.5,2.5), new Point2D(2.5,5)), tree.getQuadrant(point));
+        assertEquals(new Quadtree<Integer>(new Point2D(2.5,2.5), new Point2D(2.5,5)), tree.getQuadrant(point_1));
+        assertEquals(new Quadtree<Integer>(new Point2D(2.5,2.5), new Point2D(0,5)), tree.getQuadrant(point_2));
+        assertEquals(new Quadtree<Integer>(new Point2D(2.5,2.5), new Point2D(0,7.5)), tree.getQuadrant(point_3));
     }
 
 }
